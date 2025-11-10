f0d39dc4806b538b3bfc64c9c5cbabc5058f2ccf
wmo <jrlbidamin@gmail.com>
2025-11-10 15:08:32 -0500

    saving work

--- main.go
+++ main.go
@@ -8,4 +8,34 @@ import (
 	"os/exec"
 )
 
-func main() {}
+func main() {
+
+cmd := exec.Command("journalctl", "-f", "-o", "json")
+
+stdout, err := cmd.StdoutPipe()
+if err != nil {
+	log.Error("Error getting StdoutPipe: %v", err)
+}
+
+if err := cmd.Start(); err != nil {
+	log.Error("Error starting journalctl: %v", err)
+}
+
+// reader buffer
+reader := bufio.NewReader(stdout)
+
+log.Info("streaming logs...")
+
+for {
+	line, err := reader.ReadString('\n')
+
+	// TODO: Parse certain logs out
+	if err != nil {
+		if err == io.EOF {
+			log.Info("end of stream...")
+		} else {
+			log.Error("error reading from journalctl:", err)
+		}
+		
+}
+}
