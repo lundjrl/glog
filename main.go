52f010ac46c6ea48bbd12a0ee92cecd577ffd73d
jam <jrlbidamin@gmail.com>
2025-11-10 18:16:41 -0500

    added line print

--- main.go
+++ main.go
@@ -2,40 +2,39 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io"
-	"github.com/charmbracelet/log"
 	"os/exec"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
+	cmd := exec.Command("journalctl", "-f", "-o", "json")
 
-cmd := exec.Command("journalctl", "-f", "-o", "json")
-
-stdout, err := cmd.StdoutPipe()
-if err != nil {
-	log.Error("Error getting StdoutPipe: %v", err)
-}
-
-if err := cmd.Start(); err != nil {
-	log.Error("Error starting journalctl: %v", err)
-}
-
-// reader buffer
-reader := bufio.NewReader(stdout)
-
-log.Info("streaming logs...")
-
-for {
-	line, err := reader.ReadString('\n')
-
-	// TODO: Parse certain logs out
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		if err == io.EOF {
-			log.Info("end of stream...")
-		} else {
-			log.Error("error reading from journalctl:", err)
+		log.Error("Error getting StdoutPipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Error("Error starting journalctl: %v", err)
+	}
+
+	// reader buffer
+	reader := bufio.NewReader(stdout)
+
+	log.Info("streaming logs...")
+
+	for {
+		line, err := reader.ReadString('\n')
+		// TODO: Parse certain logs out
+		if err != nil {
+			if err == io.EOF {
+				log.Info("end of stream...")
+			} else {
+				log.Error("error reading from journalctl:", err)
+			}
 		}
-		
-}
+		log.Print(line)
+	}
 }
